8bf3a74920c2288af59e05ac2f90c1397de4c731
Eduardo <eduandres35@gmail.com>
2024-10-11 01:37:50 -0300

    Segundo commit a repositorio

--- VieiApp/views.py
+++ VieiApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from VieiApp.models import Employee
 
 # Create your views here.
+def employeeData(request):
+    empleados = Employee.objects.all()
+    data = {'empleados' : empleados}
+    return render(request, 'empleados.html', data)
+    
