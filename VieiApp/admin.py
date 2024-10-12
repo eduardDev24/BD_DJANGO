4c62104eeb793bf2cdd7d9a3998aa99960fec61e
Eduardo <eduandres35@gmail.com>
2024-10-12 00:38:28 -0300

    Commit a repositorio

--- VieiApp/admin.py
+++ VieiApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from VieiApp.models import Employee
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'email', 'fono']
+
+
 # Register your models here.
+admin.site.register(Employee, EmployeeAdmin)
